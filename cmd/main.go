5851f38ec6e171444c33f4398831e42dffd6d158
Yash Khare <yash2010118@akgec.ac.in>
2023-10-07 15:24:50 +0530

    :zap: added  basic routes and endpoints

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"github.com/gofiber/fiber/v2"
-
 	"github.com/divrhino/divrhino-trivia/database"
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	database.ConnectDb()
-
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Div Rhino Trivia App!")
-	})
+	setupRoutes(app)
+
 	app.Listen(":3000")
 }
