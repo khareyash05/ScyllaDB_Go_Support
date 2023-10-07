d6b7d6860fc68ea667f06629c0373183950bf1a5
Yash Khare <yash2010118@akgec.ac.in>
2023-10-07 09:30:23 +0530

    :sparkles: postgres integration

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,18 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+
+	"github.com/divrhino/divrhino-trivia/database"
+)
 
 func main() {
+	database.ConnectDb()
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World! YAfdsh")
+		return c.SendString("Div Rhino Trivia App!")
 	})
-
 	app.Listen(":3000")
 }
