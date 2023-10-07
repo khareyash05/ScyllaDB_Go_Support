a9e278fc3570e74380d34a7547d081d0bf4efc2a
Yash Khare <yash2010118@akgec.ac.in>
2023-10-07 15:45:47 +0530

    :art: create views for frontend

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"github.com/divrhino/divrhino-trivia/database"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html"
 )
 
 func main() {
 	database.ConnectDb()
-	app := fiber.New()
+
+	engine := html.New("./views", ".html")
+
+	app := fiber.New(fiber.Config{
+		Views:       engine,
+		ViewsLayout: "layouts/main",
+	})
 
 	setupRoutes(app)
 
--- handlers/facts.go
+++ handlers/facts.go
@@ -7,13 +7,14 @@ import (
 )
 
 func ListFacts(c *fiber.Ctx) error {
-
 	facts := []models.Fact{}
-
 	database.DB.Db.Find(&facts)
 
-	return c.Status(200).JSON(facts)
-
+	return c.Render("index", fiber.Map{
+		"Title":    "Div Rhino Trivia Time",
+		"Subtitle": "Facts for funtimes with friends!",
+		"Facts":    facts,
+	})
 }
 
 func CreateFact(c *fiber.Ctx) error {
